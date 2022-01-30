a3e9b6faa9f80ade9896cf1f8b5848836645c481
Николай Колышев <NIKolyshev@sberbank.ru>
2022-01-30 23:08:12 +0300

    update(branches-script): added removing branches

--- branches-script.js
+++ branches-script.js
@@ -3,11 +3,23 @@ import simpleGit from 'simple-git';
 const init = async () => {
     const git = await simpleGit();
 
-    const diffs = await git.log(['master...dev-1']);
     const branches = await git.branch(['-l']);
 
     //console.log(JSON.stringify(diffs, null, 4));
-    console.log(JSON.stringify(branches, null, 4));
+    //console.log(JSON.stringify(diffs, null, 4));
+
+    for (const branchName of branches?.all?.filter(branchName => branchName !== 'master' && branchName !== 'main-dev') || []) {
+        const diffs = await git.log([`master..${branchName}`]);
+        const diffsTotal = diffs.total;
+        //console.log(branchName)
+        if (diffsTotal === 0) {
+            console.log(branchName, ' - дубликат мастера');
+            await git.branch(['-d', branchName]);
+            console.warn('Ветка', branchName, 'удалена');
+        }
+    }
+
+
 
 
 }
