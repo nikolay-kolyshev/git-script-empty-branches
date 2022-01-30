325de60a99f2a7dbb205feb4ac97f812d83f3193
Николай Колышев <NIKolyshev@sberbank.ru>
2022-01-31 01:54:34 +0300

    refactor(branches-script): function deleteBranch

--- branches-script.js
+++ branches-script.js
@@ -1,47 +1,40 @@
 import simpleGit from 'simple-git';
 
-
 const MASTER_BRANCH = 'origin/master';
 const DEV_BRANCH = 'origin/main-dev';
 
-const sleep = async (ms) => new Promise((resolve) => {
+/*const sleep = async (ms) => new Promise((resolve) => {
     setTimeout(() => {resolve(ms)}, ms)
-});
+});*/
+
+const deleteBranch = (git, branchName) => {
+    return new Promise(async (resolve) => {
+        const diffs = await git.log([`${MASTER_BRANCH}..${branchName}`]);
+        const diffsTotal = diffs.total;
+        let isRemoved = false;
+        if (diffsTotal === 0) {
+            await git.push(['-d', 'origin', branchName.replace(/^origin\//, '')]);
+            isRemoved = true;
+        }
+        resolve({
+            branchName,
+            isRemoved,
+            diffsTotal,
+        });
+    });
+}
 
 const init = async () => {
+
     const git = await simpleGit();
 
     const branches = await git.branch(['-r']);
 
-    //console.log(JSON.stringify(diffs, null, 4));
-    //console.log(JSON.stringify(diffs, null, 4));
-
-    const branchCollection = branches?.all
-        ?.filter(branchName => branchName !== MASTER_BRANCH && branchName !== DEV_BRANCH)
-        ?.map((branchName, index) => new Promise(async (resolve) => {
-            const diffs = await git.log([`${MASTER_BRANCH}..${branchName}`]);
-            const diffsTotal = diffs.total;
-            //console.log(branchName)
-            let isRemoved = false;
-            if (diffsTotal === 0) {
-                await git.push(['-d', 'origin', branchName.replace(/^origin\//, '')]);
-                isRemoved = true;
-            }
-            const ms = await sleep(1000 * (index+1));
-            console.log(ms);
-            resolve({
-                branchName,
-                isRemoved,
-                diffsTotal,
-            })
-            /*resolve({
-                branchName,
-                isRemoved: false,
-                diffsTotal: 1,
-            })*/
-        })) || []
-
-    let ms = 0;
+    const branchCollection =
+        branches?.all
+            ?.filter(branchName => branchName !== MASTER_BRANCH && branchName !== DEV_BRANCH)
+            ?.map((branchName) => deleteBranch(git, branchName))
+        || [];
 
 
     for await (const brunchInfo of branchCollection) {
